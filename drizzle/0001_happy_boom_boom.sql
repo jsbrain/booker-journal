39bcb425947ad82211afcd6d85b6ba2cbe89d9fd
Manuel Maute <manuel.maute@bradbit.com>
2025-10-27 12:44:48 +0100

    chore: reset migrations

--- drizzle/0001_happy_boom_boom.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "journal_entries" ALTER COLUMN "product_id" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "shared_links" ADD COLUMN "start_date" timestamp;--> statement-breakpoint
-ALTER TABLE "shared_links" ADD COLUMN "end_date" timestamp;
